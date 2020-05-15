cddde8512fd11df94e4c3f37445176f5deac978d
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-15 10:01:47 +0530

    create PostfixEvaluator class object

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -1,11 +1,14 @@
 package execution;
 
+import defination.PostfixEvaluator;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String expression = scanner.nextLine();
+        PostfixEvaluator postfixEvaluator = new PostfixEvaluator();
 
 
     }
