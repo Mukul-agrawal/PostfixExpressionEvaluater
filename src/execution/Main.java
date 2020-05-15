90b37ce5c6a420d27c079d150a946c03519f4af9
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-15 10:04:51 +0530

    final commit

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -5,11 +5,11 @@ import defination.PostfixEvaluator;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PostfixEvaluator.SyntaxErrorException {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter postfix Expression : ");
         String expression = scanner.nextLine();
         PostfixEvaluator postfixEvaluator = new PostfixEvaluator();
-
-
+        System.out.println(postfixEvaluator.eval(expression));
     }
 }
