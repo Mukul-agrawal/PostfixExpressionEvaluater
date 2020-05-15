0a066c0141956de50dadc62d6ff4cc42dd76bdb7
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-15 09:42:55 +0530

    create abstract methods

--- src/adt/EvaluateADT.java
+++ src/adt/EvaluateADT.java
@@ -1,4 +1,11 @@
 package adt;
 
+import java.util.Deque;
+
 public interface EvaluateADT {
+    int evalOperation(char operator, Deque<Integer> operandStack);
+
+    boolean isOperator(char ch);
+
+    String eval(String expression);
 }
