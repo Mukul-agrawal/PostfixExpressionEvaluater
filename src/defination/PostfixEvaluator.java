a916f39fa940e0d754a4e42fe13a37b21c9395ad
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-15 09:44:34 +0530

    implements the EvaluateADT interface and override the methods

--- src/defination/PostfixEvaluator.java
+++ src/defination/PostfixEvaluator.java
@@ -1,4 +1,22 @@
 package defination;
 
-public class PostfixEvaluator {
+import adt.EvaluateADT;
+
+import java.util.Deque;
+
+public class PostfixEvaluator implements EvaluateADT {
+    @Override
+    public int evalOperation(char operator, Deque<Integer> operandStack) {
+        return 0;
+    }
+
+    @Override
+    public boolean isOperator(char ch) {
+        return false;
+    }
+
+    @Override
+    public String eval(String expression) {
+        return null;
+    }
 }
