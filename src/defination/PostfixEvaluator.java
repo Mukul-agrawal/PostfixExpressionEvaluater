902bf1f0bde5f1d6ccbd5b0b1a33469fb024d9c8
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-15 09:49:47 +0530

    create method body of evalOperation

--- src/defination/PostfixEvaluator.java
+++ src/defination/PostfixEvaluator.java
@@ -7,7 +7,24 @@ import java.util.Deque;
 public class PostfixEvaluator implements EvaluateADT {
     @Override
     public int evalOperation(char operator, Deque<Integer> operandStack) {
-        return 0;
+        int rhs = operandStack.pop();
+        int lhs = operandStack.pop();
+        int result = 0;
+        switch (operator) {
+            case '+':
+                result = lhs + rhs;
+                break;
+            case '‐':
+                result = lhs - rhs;
+                break;
+            case '/':
+                result = lhs / rhs;
+                break;
+            case '*':
+                result = lhs * rhs;
+                break;
+        }
+        return result;
     }
 
     @Override
